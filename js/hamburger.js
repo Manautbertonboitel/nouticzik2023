7d223b923396023b1228188919edd3ad04e167f9
Manaut <manaut.bertonboitel@outlook.fr>
2023-06-06 20:16:02 +0200

    favicon, imgs, fixes

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,8 +3,37 @@
 var hamburger = document.querySelector(".header__main__navs__button");
 var header = document.querySelector("header");
 var body = document.querySelector("body");
+var toggle1 = document.querySelector(".toggle1");
+var toggle2 = document.querySelector(".toggle2");
+var toggle3 = document.querySelector(".toggle3");
+var toggle4 = document.querySelector(".toggle4");
+
 // On click
 hamburger.addEventListener("click", function() {
+    // Toggle class "is-active" && "open"
+    header.classList.toggle("navs--collapse--open");
+    body.classList.toggle("no-scroll");
+});
+
+toggle1.addEventListener("click", function() {
+    // Toggle class "is-active" && "open"
+    header.classList.toggle("navs--collapse--open");
+    body.classList.toggle("no-scroll");
+});
+
+toggle2.addEventListener("click", function() {
+    // Toggle class "is-active" && "open"
+    header.classList.toggle("navs--collapse--open");
+    body.classList.toggle("no-scroll");
+});
+
+toggle3.addEventListener("click", function() {
+    // Toggle class "is-active" && "open"
+    header.classList.toggle("navs--collapse--open");
+    body.classList.toggle("no-scroll");
+});
+
+toggle4.addEventListener("click", function() {
     // Toggle class "is-active" && "open"
     header.classList.toggle("navs--collapse--open");
     body.classList.toggle("no-scroll");
